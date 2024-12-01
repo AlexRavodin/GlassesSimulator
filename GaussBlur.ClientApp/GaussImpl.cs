eec5f6e7576dca95de5cb08bd2b9fd9f1080c829
Aleksandr Ravodin <91945858+Bel1eveme@users.noreply.github.com>
2024-12-02 01:01:50 +0300

    init

---
+++ GaussBlur.ClientApp/GaussImpl.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GaulBlur.App;
+
+public class GaussianBlur
+{
+    private readonly int[] _alpha;
+    private readonly int[] _red;
+    private readonly int[] _green;
+    private readonly int[] _blue;
+
+    private readonly int _width;
+    private readonly int _height;
+
+    private readonly ParallelOptions _pOptions = new() { MaxDegreeOfParallelism = 16 };
+
+    private readonly ArrayPool<int> _arrayPool = ArrayPool<int>.Shared;
+
+    public GaussianBlur(BitmapImage image)
+    {
+        _width = image.PixelWidth;
+        _height = image.PixelHeight;
+
+        var pixelData = _arrayPool.Rent(_width * _height);
+        image.CopyPixels(new Int32Rect(0, 0, _width, _height), pixelData, _width * 4, 0);
+        
+        _alpha = _arrayPool.Rent(_width * _height);
+        _red = _arrayPool.Rent(_width * _height);
+        _green = _arrayPool.Rent(_width * _height);
+        _blue = _arrayPool.Rent(_width * _height);
+
+        Parallel.For(0, pixelData.Length, i =>
+        {
+            _alpha[i] = (pixelData[i] >> 24) & 0xff;
+            _red[i] = (pixelData[i] >> 16) & 0xff;
+            _green[i] = (pixelData[i] >> 8) & 0xff;
+            _blue[i] = pixelData[i] & 0xff;
+        });
+        
+        _arrayPool.Return(pixelData);
+    }
+
+    public BitmapSource Process(int radial)
+    {
+        var newAlpha = _arrayPool.Rent(_width * _height);
+        var newRed = _arrayPool.Rent(_width * _height);
+        var newGreen = _arrayPool.Rent(_width * _height);
+        var newBlue = _arrayPool.Rent(_width * _height);
+        var dest = _arrayPool.Rent(_width * _height);
+
+        Parallel.Invoke(
+            () => gaussBlur_4(_alpha, newAlpha, radial),
+            () => gaussBlur_4(_red, newRed, radial),
+            () => gaussBlur_4(_green, newGreen, radial),
+            () => gaussBlur_4(_blue, newBlue, radial));
+
+        Parallel.For(0, dest.Length, i =>
+        {
+            newAlpha[i] = Math.Clamp(newAlpha[i], 0, 255);
+            newRed[i] = Math.Clamp(newRed[i], 0, 255);
+            newGreen[i] = Math.Clamp(newGreen[i], 0, 255);
+            newBlue[i] = Math.Clamp(newBlue[i], 0, 255);
+
+            dest[i] = (newAlpha[i] << 24) | (newRed[i] << 16) | (newGreen[i] << 8) | (newBlue[i]);
+        });
+
+        var bitmapSource = BitmapSource.Create(_width, _height, 96, 96,
+            System.Windows.Media.PixelFormats.Bgra32,
+            null,
+            dest,
+            _width * 4);
+        
+        _arrayPool.Return(newAlpha);
+        _arrayPool.Return(newRed);
+        _arrayPool.Return(newGreen);
+        _arrayPool.Return(newBlue);
+        _arrayPool.Return(dest);
+        
+        _arrayPool.Return(_alpha);
+        _arrayPool.Return(_red);
+        _arrayPool.Return(_green);
+        _arrayPool.Return(_blue);
+
+        return bitmapSource;
+    }
+
+    private void gaussBlur_4(int[] source, int[] dest, int r)
+    {
+        var bxs = BoxesForGauss(r, 3);
+        boxBlur_4(source, dest, _width, _height, (bxs[0] - 1) / 2);
+        boxBlur_4(dest, source, _width, _height, (bxs[1] - 1) / 2);
+        boxBlur_4(source, dest, _width, _height, (bxs[2] - 1) / 2);
+    }
+
+    private static Span<int> BoxesForGauss(int sigma, int n)
+    {
+        var wIdeal = Math.Sqrt(12 * sigma * sigma / n + 1);
+        var wl = (int)Math.Floor(wIdeal);
+        if (wl % 2 == 0) wl--;
+        var wu = wl + 2;
+
+        var mIdeal = (double)(12 * sigma * sigma - n * wl * wl - 4 * n * wl - 3 * n) / (-4 * wl - 4);
+        var m = Math.Round(mIdeal);
+
+        var sizes = new int[n];
+        for (var i = 0; i < n; i++) sizes[i] = (i < m ? wl : wu);
+        return sizes.AsSpan();
+    }
+
+    private void boxBlur_4(int[] source, int[] dest, int w, int h, int r)
+    {
+        for (var i = 0; i < source.Length; i++) dest[i] = source[i];
+        boxBlurH_4(dest, source, w, h, r);
+        boxBlurT_4(source, dest, w, h, r);
+    }
+
+    private void boxBlurH_4(int[] source, int[] dest, int w, int h, int r)
+    {
+        var iar = (double)1 / (r + r + 1);
+        Parallel.For(0, h, _pOptions, i =>
+        {
+            var ti = i * w;
+            var li = ti;
+            var ri = ti + r;
+            var fv = source[ti];
+            var lv = source[ti + w - 1];
+            var val = (r + 1) * fv;
+            for (var j = 0; j < r; j++) val += source[ti + j];
+            for (var j = 0; j <= r; j++)
+            {
+                val += source[ri++] - fv;
+                dest[ti++] = (int)Math.Round(val * iar);
+            }
+
+            for (var j = r + 1; j < w - r; j++)
+            {
+                val += source[ri++] - dest[li++];
+                dest[ti++] = (int)Math.Round(val * iar);
+            }
+
+            for (var j = w - r; j < w; j++)
+            {
+                val += lv - source[li++];
+                dest[ti++] = (int)Math.Round(val * iar);
+            }
+        });
+    }
+
+    private void boxBlurT_4(int[] source, int[] dest, int w, int h, int r)
+    {
+        var iar = (double)1 / (r + r + 1);
+        Parallel.For(0, w, _pOptions, i =>
+        {
+            var ti = i;
+            var li = ti;
+            var ri = ti + r * w;
+            var fv = source[ti];
+            var lv = source[ti + w * (h - 1)];
+            var val = (r + 1) * fv;
+            for (var j = 0; j < r; j++) val += source[ti + j * w];
+            for (var j = 0; j <= r; j++)
+            {
+                val += source[ri] - fv;
+                dest[ti] = (int)Math.Round(val * iar);
+                ri += w;
+                ti += w;
+            }
+
+            for (var j = r + 1; j < h - r; j++)
+            {
+                val += source[ri] - source[li];
+                dest[ti] = (int)Math.Round(val * iar);
+                li += w;
+                ri += w;
+                ti += w;
+            }
+
+            for (var j = h - r; j < h; j++)
+            {
+                val += lv - source[li];
+                dest[ti] = (int)Math.Round(val * iar);
+                li += w;
+                ti += w;
+            }
+        });
+    }
+}
---
+++ GaussBlur.ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO.Ports;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace GaulBlur.App;
+
+public partial class MainWindow : Window
+{
+    private BitmapImage? _blurredBitmapImage;
+    private BitmapImage? _normalBitmapImage;
+
+    private Uri? _fileUri;
+
+    private const double MoveAmount = 10;
+    private const int GaussRadiusIncrement = 1;
+    private int _gaussRadious = 5;
+
+    private readonly SerialPort _port;
+    private readonly object _syncObject = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Canvas.SetLeft(LeftGlass, 500);
+        Canvas.SetTop(LeftGlass, 100);
+        Canvas.SetLeft(RightGlass, 300);
+        Canvas.SetTop(RightGlass, 100);
+
+        _port = new SerialPort("COM12", 115200);
+        _port.DataReceived += DataReceivedHandler;
+    }
+
+    private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new()
+        {
+            Title = "Select an Image",
+            Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*"
+        };
+
+        if (openFileDialog.ShowDialog() != true) return;
+
+        _fileUri = new Uri(openFileDialog.FileName);
+        _blurredBitmapImage = new BitmapImage(_fileUri);
+        var gauss = new GaussianBlur(_blurredBitmapImage);
+        MainImageView.Source = gauss.Process(_gaussRadious);
+
+        _normalBitmapImage = new BitmapImage(_fileUri);
+        OverlapImageView.Source = _normalBitmapImage;
+
+        MainImageView.Loaded += (sender, args) => { UpdateClip(); };
+        OverlapImageView.Loaded += (sender, args) => { UpdateClip(); };
+
+        //_port.DiscardInBuffer();
+        //_port.Open();
+    }
+
+    private void IncreaseBlur()
+    {
+        _gaussRadious += GaussRadiusIncrement;
+        UpdateBlur();
+    }
+
+    private void DecreaseBlur()
+    {
+        _gaussRadious -= GaussRadiusIncrement;
+        UpdateBlur();
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.D1:
+                IncreaseBlur();
+                break;
+            case Key.D2:
+                DecreaseBlur();
+                return;
+            case Key.Left:
+                MoveGlass(LeftGlass, -MoveAmount, 0);
+                MoveGlass(RightGlass, -MoveAmount, 0);
+                break;
+            case Key.Right:
+                MoveGlass(LeftGlass, MoveAmount, 0);
+                MoveGlass(RightGlass, MoveAmount, 0);
+                break;
+            case Key.Up:
+                MoveGlass(LeftGlass, 0, -MoveAmount);
+                MoveGlass(RightGlass, 0, -MoveAmount);
+                break;
+            case Key.Down:
+                MoveGlass(LeftGlass, 0, MoveAmount);
+                MoveGlass(RightGlass, 0, MoveAmount);
+                break;
+        }
+
+        UpdateClip();
+    }
+
+    private void UpdateBlur()
+    {
+        if (_fileUri is null)
+        {
+            return;
+        }
+
+        _blurredBitmapImage = new BitmapImage(_fileUri);
+        var gauss = new GaussianBlur(_blurredBitmapImage);
+        MainImageView.Source = gauss.Process(_gaussRadious);
+    }
+
+    private void MoveGlass(Ellipse glass, double offsetX, double offsetY)
+    {
+        double currentLeft = Canvas.GetLeft(glass);
+        double currentTop = Canvas.GetTop(glass);
+        Canvas.SetLeft(glass, currentLeft + offsetX);
+        Canvas.SetTop(glass, currentTop + offsetY);
+    }
+
+    private void UpdateClip()
+    {
+        var leftGlassCenter = new Point(Canvas.GetLeft(LeftGlass) + LeftGlass.Width / 2,
+            Canvas.GetTop(LeftGlass) + LeftGlass.Height / 2);
+        var rightGlassCenter = new Point(Canvas.GetLeft(RightGlass) + RightGlass.Width / 2,
+            Canvas.GetTop(RightGlass) + RightGlass.Height / 2);
+
+        var glass1 = new EllipseGeometry(leftGlassCenter, LeftGlass.Width / 2, LeftGlass.Height / 2);
+        var glass2 = new EllipseGeometry(rightGlassCenter, RightGlass.Width / 2, RightGlass.Height / 2);
+
+        var glasses = new CombinedGeometry(GeometryCombineMode.Union, glass1, glass2);
+
+        var background = new RectangleGeometry(new Rect(0, 0, MainImageView.ActualWidth, MainImageView.ActualHeight));
+
+        var fullPicture = new CombinedGeometry(GeometryCombineMode.Exclude, background, glasses);
+
+        MainImageView.Clip = fullPicture;
+    }
+
+    private void DataReceivedHandler(object data, SerialDataReceivedEventArgs args)
+    {
+        const int intSize = 4;
+
+        lock (_syncObject)
+        {
+            var count = _port.BytesToRead;
+            var intsCount = count / intSize;
+
+            byte[] byteArray = new byte[intsCount * 4];
+            _port.Read(byteArray, 0, intsCount * 4);
+
+            Span<byte> byteSpan = byteArray.AsSpan();
+
+            Console.WriteLine($"Unread count: {count - intsCount * 4}");
+
+            for (int i = 0; i < intsCount * 4; i += 4)
+            {
+                var value = BinaryPrimitives.ReadInt32LittleEndian(byteSpan.Slice(i));
+                Console.WriteLine($"Values: {value}");
+                Console.WriteLine($"HEX: {value:x8}");
+
+                if (true)
+                {
+                    UpdateClip();
+                }
+                else
+                {
+                    if (true)
+                    {
+                        IncreaseBlur(); 
+                    }
+                    else
+                    {
+                        DecreaseBlur();
+                    }
+                }
+            }
+        }
+    }
+}
